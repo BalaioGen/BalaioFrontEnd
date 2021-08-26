923ed51e576168de7e71a3eb75d7cf2212444dd4
laisassis <laisflor1003@gmail.com>
2021-08-26 16:08:42 -0300

    Rodapé-Menu-Cadastro-Entrar-Home-Sobre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { EntrarComponent } from './entrar/entrar.component';
+const routes: Routes = [
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'entrar', component: EntrarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    CadastroComponent,
+    HomeComponent,
+    SobreComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,

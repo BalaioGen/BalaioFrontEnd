175fe9eae8fb11408d78f6368e918ac474bd03d9
Matheus Tabarelli <12tabarelli@gmail.com>
2021-09-10 17:24:18 -0300

    Corrigindo Issues

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { CategoriaModel } from '../model/CategoriaModel';
 import { ProdutoModel } from '../model/ProdutoModel';
+import { CategoriasService } from '../service/categorias.service';
 import { ProdutosService } from '../service/produtos.service';
 
 
@@ -14,30 +16,44 @@ export class ProdutosComponent implements OnInit {
 
   produtos: ProdutoModel = new ProdutoModel()
   listaProdutos: ProdutoModel[]
+  categoria: CategoriaModel = new CategoriaModel()
+  listaCategorias: CategoriaModel[]
 
   constructor(
-    private router: Router, private ProdutosService: ProdutosService
+    private router: Router,
+    private ProdutosService: ProdutosService,
+    private ctagoriasService: CategoriasService
   ) { }
 
   ngOnInit() {
-  //   if(environment.token == ""){
-  //     this.router.navigate(['/entrar'])
-  //   }
-  //   this.getAllProdutos()
-  this.getAllProdutos();  
-}
-  getAllProdutos(){
-    this.ProdutosService.getAllProdutos().subscribe((resp: ProdutoModel[])=>{
+    //   if(environment.token == ""){
+    //     this.router.navigate(['/entrar'])
+    //   }
+    //   this.getAllProdutos()
+    this.getAllProdutos();
+  }
+  getAllProdutos() {
+    this.ProdutosService.getAllProdutos().subscribe((resp: ProdutoModel[]) => {
       this.listaProdutos = resp
     })
- }
+  }
 
-//  cadastrarProdutos(){
-//    this.ProdutosService.postProdutos(this.produtos).subscribe((resp: ProdutoModel) => {
-//      this.produtos=resp
-//      alert('Produto Cadastrado com sucesso!')
-//      this.getAllProdutos()
-//      this.produtos = new ProdutoModel()
-//    })
-//  }
+  getCategoriaById(id: number) {
+    this.ctagoriasService.getCategoriasById(id).subscribe((resp: CategoriaModel) => {
+      this.categoria = resp
+      console.log(this.categoria)
+    })
+
+  }
+
+
+
+  //  cadastrarProdutos(){
+  //    this.ProdutosService.postProdutos(this.produtos).subscribe((resp: ProdutoModel) => {
+  //      this.produtos=resp
+  //      alert('Produto Cadastrado com sucesso!')
+  //      this.getAllProdutos()
+  //      this.produtos = new ProdutoModel()
+  //    })
+  //  }
 }
